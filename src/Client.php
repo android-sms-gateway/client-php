402c8802f48db39c1f06aec20e8c28753810ba93
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-01-31 09:07:39 +0700

    Fixed: client version

--- src/Client.php
+++ src/Client.php
@@ -13,7 +13,7 @@ use AndroidSmsGateway\Domain\MessageState;
 
 class Client {
     public const DEFAULT_URL = 'https://sms.capcom.me/api/3rdparty/v1';
-    public const USER_AGENT_TEMPLATE = 'android-sms-gateway/1.0 (client; php %s)';
+    public const USER_AGENT_TEMPLATE = 'android-sms-gateway/1.1 (client; php %s)';
 
     protected string $basicAuth;
     protected string $baseUrl;
